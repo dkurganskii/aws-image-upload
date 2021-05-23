520ef1680a24b2823a3cbbee8d74d68cbb96c3b9
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-05-23 07:51:11 -0500

    add components

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from "react"
 import logo from './logo.svg';
 import './App.css';
-import axios from "aria-query"
+import axios from "axios"
+
+const UserProfiles = () => {
+  const fetchUserProfiles = () => {
+    axios.get("http://localhost:8010/api/v1/user-profile").then(res => {
+      console.log(res)
+    })
+  }
+  useEffect(() => {
+    fetchUserProfiles()
+  }, [])
+  return <h1>Hello</h1>
+}
 
 function App() {
   return (
     <div className="App">
-
+      <UserProfiles />
     </div>
   );
 }
