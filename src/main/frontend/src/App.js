b5add0115f9c1e3bce001341f0665a01a3f5c0de
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2021-05-23 08:02:32 -0500

    render user profile

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,18 +1,28 @@
 import React, { useState, useEffect } from "react"
-import logo from './logo.svg';
 import './App.css';
 import axios from "axios"
 
 const UserProfiles = () => {
+  const [userProfiles, setUserProfiles] = useState([])
   const fetchUserProfiles = () => {
     axios.get("http://localhost:8010/api/v1/user-profile").then(res => {
       console.log(res)
+      setUserProfiles(res.data)
     })
   }
+
   useEffect(() => {
     fetchUserProfiles()
   }, [])
-  return <h1>Hello</h1>
+
+  return userProfiles.map((userProfile, index) => {
+    return (
+      <div key={index}>
+        <h1>{userProfile.username}</h1>
+        <p>{userProfile.userProfileId}</p>
+      </div>
+    )
+  })
 }
 
 function App() {
